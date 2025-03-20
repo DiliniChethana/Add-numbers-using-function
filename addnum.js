423594abcf91e8c256e18e3ef85177ebf0b56ec6
DiliniChethana <167061217+DiliniChethana@users.noreply.github.com>
2025-03-20 22:03:39 +0530

    committed

---
+++ addnum.js
@@ -0,0 +1,6 @@
+function addnum(){
+    let firname=document.getElementById("fnum").Value;
+    let secname=document.getElementById("snum").Value;
+    let answer= (fnum + snum);
+    document.getElementById("finalanswer").innerHTML="Answer is " +answer;
+}
